50f49284bacf8c15f765aaff8fa988e81e90d9b0
Nicholas Reich <nick@umass.edu>
2023-05-17 16:07:24 -0400

    adding a multi-date example.

--- _targets.R
+++ _targets.R
@@ -2,9 +2,13 @@
 
 # Load packages required to define the pipeline:
 library(targets)
+library(tarchetypes)
+library(tibble)
+library(crew)
 
 # Set target options:
 tar_option_set(
+  controller = crew_controller_local(workers = 4),
   packages = c("tidyverse", "covidHubUtils", "distfromq", "alloscore", "gh"), # packages that your targets need to run
   format = "rds" # default storage format
 )
@@ -14,17 +18,17 @@ tar_option_set(
 # remotes::install_github("reichlab/distfromq")
 # remotes::install_github("reichlab/covidHubUtils")
 
-# tar_make_clustermq() configuration (okay to leave alone):
-options(clustermq.scheduler = "multicore")
-
 # Run the R scripts in the R/ folder:
 tar_source()
+
+## create a group of alloscore targets
+values <- tibble(forecast_dates = as.character(seq.Date(as.Date("2021-12-13"), as.Date("2021-12-27"), by = "7 days")))
 
 # List of targets:
 list(
   tar_target(
     name = forecast_data,
-    command = get_forecast_data("2021-12-27")
+    command = get_forecast_data(values$forecast_dates)
   ),
   tar_target(
     name = truth_data,
@@ -34,13 +38,15 @@ list(
     name = score_data,
     command = get_forecast_scores(forecast_data, truth_data)
   ),
-  tar_target(
-    name = alloscores,
-    command = run_alloscore(forecast_data, truth_data)
+  tar_map(
+    values = values,
+    tar_target(alloscore, run_alloscore(forecast_data, truth_data, forecast_dates))
   ),
   tar_target(
-    name = figure_K_v_alloscore,
-    command = plot_K_v_alloscore(alloscores),
+    name = figure_K_v_alloscore_2021.12.13,
+    command = plot_K_v_alloscore(alloscore_2021.12.13),
     format = "file"
   )
+
+
 )
