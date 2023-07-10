e63881476d5fcd0be1ffa0e64824d6cdcaf9eb72
Aaron Gerding <aaronger@users.noreply.github.com>
2023-07-10 00:35:32 -0400

    fix iter_pic, start allocation score discussion for paper intro

--- random_undocumented_experiments/iter_pic.R
+++ random_undocumented_experiments/iter_pic.R
@@ -1,125 +1,35 @@
 library(tidyverse)
 library(patchwork)
 library(devtools)
-load_all()
+library(alloscore)
+source("random_undocumented_experiments/iter_pic_funs.R")
 
-(dat <- tibble(
-  alpha = c(.2,.6,.3,.9,.5),
-  w = c(3,2,3,5,2),
-  dists_and_params = list(
-    list(dist = "norm", mean = 3, sd = 1),
-    list(dist = "norm", mean = 2, sd = 4),
-    list(dist = "exp", rate = .5),
-    list(dist = "gamma", shape = 1, rate = .3),
-    list(dist = "unif", min = 5, max = 6)
-    #list(dist = "distfromq", ps = c(.1,.3,.6,.9,.95), qs = c(2,3,3,4,5))
-  ),
-  name = map(dists_and_params,
-             ~toString(paste0(.[["dist"]], ", ",
-                              toString(paste(names(.)[-1], .[-1], sep = "=")))))) %>%
-  unnest_wider(col = dists_and_params) %>%
-  add_pdqr_funs(types = c("p", "q")) %>% mutate(
-    q_scale = map2_dbl(Q, alpha, exec),
-    # add marginal expected benefit functions scaled to fit on single [0,1] interval,
-    Lambda_scaled = pmap(., margexb_fun),
-    # and unscaled as well, which are not yet being used
-    Lambda = pmap(.[names(.) != "q_scale"], margexb_fun),
-    Eloss = pmap(.[c("alpha", "F")], gpl_loss_exp_fun)
-  )
-)
+make_iter_pic(K = 2,
+              alpha = c(.2,.6,.3,.9,.5),
+              w = c(3,2,3,5,2),
+              dists_and_params = list(
+                list(dist = "norm", mean = 3, sd = 1),
+                list(dist = "norm", mean = 2, sd = 4),
+                list(dist = "exp", rate = .5),
+                list(dist = "gamma", shape = 1, rate = .3),
+                list(dist = "unif", min = 5, max = 6)
+                #list(dist = "distfromq", ps = c(.1,.3,.6,.9,.95), qs = c(2,3,3,4,5))
+              ))
 
-make_iter_pic(2, dat)
+make_iter_pic(K = 20,
+              dists_and_params = list(
+                list(dist = "unif", min = 0, max = 7),
+                list(dist = "unif", min = 7, max = 15)
+              ))
 
-make_iter_pic <- function(K, dat) {
-  (dat_allo <- dat %>%
-     mutate(
-       allo = allocate(
-         F = F,
-         Q = Q,
-         alpha = alpha,
-         w = w,
-         K = K,
-         eps_K = .01,
-         eps_lam = .0001
-       ),
-       allo_q_scaled = allo/q_scale
-     ) %>%
-     relocate(allo, allo_q_scaled, q_scale)
-  )
+dat <- make_param_df(dists_and_params = list(
+  list(dist = "unif", min = 0, max = 7),
+  list(dist = "unif", min = 7, max = 15)
+))
 
-  # get all iterations of lambda from allocation algorithm
-  long <- with(dat_allo,
-               allocate(
-                 F = F,
-                 Q = Q,
-                 alpha = alpha,
-                 w = w,
-                 K = K,
-                 eps_K = .01,
-                 eps_lam = .0001,
-                 Trace = TRUE
-               ))
-  lam_iters <- long$lambdas
+dat_allo <- dat %>% allocate(K=20)
 
-
-  # make function of lambda that gives total resource use at "lambda-quantiles"
-  # which are regular quantiles when lambda = 0.
-  # When `!is.null(forecasts))`, the total use is for the subset of rows given by
-  # vector `forecasts`
-  K_fun_factory <- function(df = dat_allo, forecasts = NULL) {
-    if (!is.null(forecasts)) df <- df[forecasts,]
-    Vectorize(with(df, function(lambda) {
-      alpha_lam <- alpha-w*lambda
-      positives <- alpha_lam>0
-      qs_plus <- pmax(0, map2_dbl(Q, alpha_lam*positives, exec))
-      qs <- qs_plus*positives
-      sum(w * qs)
-    }))
-  }
-  K_fun <- K_fun_factory()
-
-  ymax = max(map_dbl(dat_allo$Lambda, ~.(0)))*(1.1)
-  xmax = max(dat_allo$allo)*1.1
-  n = nrow(dat_allo)
-  with(dat_allo,
-       {
-         p1 <- ggplot() + xlim(c(0,xmax)) + ylim(0, ymax) +
-           geom_hline(yintercept = 0) +
-           map(lam_iters, ~ geom_hline(yintercept = ., alpha = .3)) +
-           geom_label(data = enframe(lam_iters[2:5]), aes(x=0,y=value,label=name)) +
-           map(1:n, ~ geom_function(aes(color = name[[.]]), fun = Lambda[[.]])) +
-           map(1:n, ~ geom_vline(aes(xintercept = allo[.], color = name[[.]]), alpha=.5)) +
-           labs(color = "Distribution",
-                y = expression(lambda(x)),
-                x = expression(x[i])) +
-           theme_classic() +
-           theme(legend.position = "left")
-
-         p2 <- ggplot(data = tibble(y = seq(0, 1.01, length.out = 1000), x = K_fun(y)),
-                      aes(x = x, y = y)) +
-           ylim(0, ymax) + xlim(0, K_fun(0.01)) +
-           geom_line() +
-           geom_label(data = enframe(lam_iters[2:5]) %>% mutate(x = K_fun(value)), aes(x=x,y=value,label=name)) +
-           # geom_line(aes(x =  K_funfac(forecasts = c(2, 4))(y)), color = "blue") +
-           # #geom_line(aes(x =  K_funfac(forecasts = c(1,2,4))(y))) +
-           #map(1:n, ~ geom_line(aes(x = K_funfac(forecasts = c(.))(y), color = name[[.]]))) +
-           geom_vline(xintercept = K) +
-           labs(x = expression(w ^ T * Q(alpha - w * lambda))) +
-           theme(
-             axis.text.y = element_blank(),
-             axis.ticks.y = element_blank(),
-             axis.title.y = element_blank()
-           )+
-           theme(legend.position = "none")
-
-         p1+p2
-       })
-}
-
-
-
-
-# Plot MEBs
+  # Plot MEBs
 # with(dat,
 #      {p <- ggplot() + xlim(-1, 10) +
 #          map(1:nrow(dat), ~ geom_function(aes(color = name[[.]]), fun = Lambda[[.]]))
@@ -133,15 +43,15 @@ plotdat <- function(dat) {
   p <- ggplot(data = data.frame(
     x = seq(from = xl, to = xr, length.out = 1000)
   ))
-  p <- p + map(1:nrow(plt_dat), function(i) geom_line(
-    aes(x = x, y = plt_dat$F[[i]](x), color = plt_dat$name[[i]]), linetype = 5))
-  p_lam <- p + map(1:nrow(plt_dat), function(i) geom_line(
-    aes(x = x, y = plt_dat$Lambda[[i]](x), color = plt_dat$name[[i]])))
+  # p <- p + map(1:nrow(plt_dat), function(i) geom_line(
+  #   aes(x = x, y = plt_dat$F[[i]](x), color = plt_dat$name[[i]]), linetype = 5))
+  # p_lam <- p + map(1:nrow(plt_dat), function(i) geom_line(
+  #   aes(x = x, y = plt_dat$Lambda[[i]](x), color = plt_dat$name[[i]])))
   #p_lam <- p_lam+ylim(c(-1,1))
   p_loss <- p + map(1:nrow(plt_dat), function(i) geom_line(
     aes(x = x, y = plt_dat$Eloss[[i]](x), color = plt_dat$name[[i]])))
-  #p_loss <- p_loss+ylim(c(0,2))
-  p_lam + p_loss + plot_layout(ncol=1)
+  p_loss
+  #p_lam + p_loss + plot_layout(ncol=1)
 }
 
 plotdat(dat)
--- talks/ISF_talk/iter_plot.R
+++ talks/ISF_talk/iter_plot.R
@@ -2,6 +2,7 @@ library(scales)
 library(targets)
 adf <- tar_read(alloscore_BPagano.RtDriven_2022.01.03)
 
+source("_targets.R")
 tar_load(truth_data)
 tar_load(forecast_data)
 
@@ -11,7 +12,7 @@ require(tidyverse)
 require(alloscore)
 require(distfromq)
 
-one_model <- mkeep[4]
+one_model <- mkeep[1]
 one_reference_date <- "2021-12-27"
 
 ## process forecast data, adding distfromq output
@@ -45,10 +46,10 @@ dist_plot_dat <- forecast_data_processed %>% select(abbreviation, F) %>%
            y_values = map2(F, x_values, function(F, xl) F(xl))) %>%
   select(-F) %>% unnest(-abbreviation)
 
-dist_plot <- dist_plot_dat %>%
+dist_plot <- dist_plot_dat %>% filter(abbreviation == "AK") %>%
   ggplot(aes(x = x_values, y = y_values, color = abbreviation)) + geom_line()
 
-dist_plot + xlim(c(0, 1000))
+dist_plot + xlim(c(0, 5))
 
 # with quantiles for a subset of states
 
@@ -108,36 +109,56 @@ sc = 1.4
 ggsave(plot = dist_pqs_plot_OH, filename = "dist_pqs_plot_OH.png",
        width = 7*sc, height = 3*sc, path = save_dir)
 
-  ## run alloscore
-one_K = 10000
 
+plot_iterations <- function(
+    one_K = 10000,
+    model_name = one_model,
+    itnum = NULL,
+    states_to_keep = 6,
+    default_colors = scales::hue_pal()(l)[sample(1:l, l)]) {
+  ## run alloscore
   ascores <- forecast_data_processed %>%
     alloscore(
-      starting_K = 60000,
       K = one_K,
       y = .[["value"]],
       target_names = "abbreviation"
     )
   ascores <- ascores %>% dplyr::mutate(
     reference_date = one_reference_date,
-    model = one_model,
+    model = model_name,
     .before = 1
   )
 
 # make a plot of the xs iterations for one model across multiple dates
 
-iters_bp <- ascores %>% filter(model == one_model, K == one_K) %>%
+ascores <- ascores %>% filter(model == model_name, K == one_K)
+
+iters_bp <- ascores %>%
   select(model, reference_date, K, xs) %>% unnest(xs) %>%
   mutate(init_alloc = ntile(`1`, 7), .after = abbreviation)
 
-itnum <- "14"
-states_to_keep <- 6
+if (is.null(itnum)) {
+  itnum <- last(names(iters_bp))
+}
+
+lambdas <- ascores %>%
+  select(lamL_seq, lamU_seq, lam_seq) %>%
+  unnest(cols = c(lamL_seq, lamU_seq, lam_seq)) %>%
+  mutate(
+    lamL_seq = lag(lamL_seq, 1),
+    lamU_seq = lag(lamU_seq, 1),
+  ) %>%
+  slice(1:(as.numeric(itnum)+1)) %>%
+  mutate(
+    iteration = row_number() - 1,
+    data_type = "lambda")
+
 keep_states <- iters_bp %>% filter(reference_date==min(reference_date)) %>%
   filter(min_rank(desc(.data[[itnum]])) <= states_to_keep) %>% pull(abbreviation)
 
 plot_iter_dat <- iters_bp %>%
   select(1:.data[[itnum]]) %>%
-  select(-`0`) %>%
+  #select(-`0`) %>%
   mutate(
     allo_rank = min_rank(desc(.data[[itnum]])), .before = 1) %>%
   arrange(allo_rank) %>%
@@ -153,34 +174,44 @@ plot_iter_dat <- iters_bp %>%
     cols = -c(1:init_alloc),
     names_to = "iteration",
     values_to = "allocation") %>%
-  mutate(iteration = as.numeric(iteration))
+  mutate(iteration = as.numeric(iteration)) %>%
+  mutate(data_type = "allocation")
 
 State_nms <- unique(plot_iter_dat$States)
 l <- length(State_nms)
-# Get the default color palette
-default_colors <- scales::hue_pal()(l)[sample(1:l, l)]
 
 # Create a named vector of colors
 color_palette <- setNames(default_colors, State_nms)
 color_palette["other"] <- "lightgrey"
 
 p_iter <- plot_iter_dat %>%
-  ggplot(aes(x = iteration, y = allocation, group = abbreviation)) +
+  ggplot() +
   #geom_point(aes(color =abbreviation, alpha = 1-.5*is.na(States))) +
-  geom_area(aes(group = abbreviation),
-            position = "stack", color = "black", size = .3, fill = NA) +
-  geom_area(aes(fill = States, alpha = 1-.5*(States == "other")),
+  geom_area(aes(group = abbreviation, x = iteration, y = allocation),
+            position = "stack", color = "black", linewidth = .3, fill = NA) +
+  geom_area(aes(group = abbreviation, x = iteration, y = allocation,
+                fill = States, alpha = 1-.5*(States == "other")),
             position = "stack") +
-  geom_hline(aes(yintercept = K), alpha = .5)+
     facet_grid(cols = vars(reference_date)) +
-  geom_vline(aes(xintercept = iteration), alpha = .2) +
   scale_alpha_continuous(guide = "none") +
   geom_text(aes(x = as.integer(itnum), y = K, label = paste("constraint =", K)),
             hjust = 1, vjust = -1, size = 3) +
   scale_x_continuous(breaks = 1:as.integer(itnum), expand = c(0, 0)) +
   scale_y_continuous(expand = expansion(mult = c(0, 0.1))) +
   scale_fill_manual(values = color_palette) + theme_classic() +
-  labs(y = "Allocations")
+  labs(y = "Allocations") +
+  geom_line(data = lambdas, aes(x = iteration, y = lam_seq)) +
+  geom_ribbon(data = lambdas, aes(x = iteration, ymin = lamL_seq, ymax = lamU_seq), alpha = .5) +
+  geom_vline(aes(xintercept = iteration), alpha = .2) +
+  geom_vline(data = lambdas, aes(xintercept = iteration), alpha = .2) +
+  geom_hline(aes(yintercept = K), alpha = .5) +
+  facet_grid(rows = vars(data_type), scales = "free_y")
+p_iter
+}
+
+plot_iterations(one_K = 40000)
+
+
 
 sc = .85
 ggsave(plot = p_iter, filename = "p_iter.png",
