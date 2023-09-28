29c78a4c65b3c10ba00b5c999ebfa794c02f7c6a
Aaron Gerding <aaronger@users.noreply.github.com>
2023-09-27 21:07:18 -0400

    intro opportunity loss; fix WIS derivation; add copula in Norm1.R

--- random_undocumented_experiments/Norm1.R
+++ random_undocumented_experiments/Norm1.R
@@ -3,13 +3,14 @@ library(tidyverse)
 library(scoringRules)
 library(patchwork)
 library(ggridges)
+library(copula)
 source("random_undocumented_experiments/scoring_helpers.R")
 
 dgp_params <- tibble(
   model = "dgp",
   target_names = paste0("site", 1:2),
   dist = "norm",
-  mean = c(200, 100),
+  mean = c(200, 180),
   sd = 50)
 
 dgp <- dgp_params %>%
@@ -20,8 +21,9 @@ y_gen <- function() {with(dgp, map2_dbl(r, 1, exec) %>% set_names(target_names))
 
 modA <- dgp_params %>% mutate(
   model = "A",
-  mean = mean[c(2,1)],
-  sd = 10) %>%
+  mean = c(190, 190),
+  #mean = mean[c(2,1)],
+  sd = 20) %>%
   add_pdqr_funs() %>%
   add_crps_funs()
 
@@ -43,7 +45,37 @@ mods <- bind_rows(dgp, modA, modB, modC)
 # select(mods, model, target_names, f) %>%
 #   pivot_wider(values_from = f, names_from = c(model, target_names))
 
-ys <- tibble(samp = 1:200, ys = map(seq_along(samp), ~y_gen()))
+ys <- tibble(samp = 1:1000, ys = map(seq_along(samp), ~y_gen()))
+
+correlate_samples <- function(ys, rho = .5) {
+  # Extract site1 and site2 values from ys
+  Y1 <- sapply(ys, function(x) x["site1"])
+  Y2 <- sapply(ys, function(x) x["site2"])
+
+  # Transform data to have uniform marginals using empirical CDF
+  pY1 <- ecdf(Y1)
+  pY2 <- ecdf(Y2)
+  U1 <- pY1(Y1)
+  U2 <- pY2(Y2)
+
+  # Fit a copula; for simplicity, we'll use a Clayton copula
+  gcop <- normalCopula(param = rho, dim = 2)
+  fit <- fitCopula(gcop, cbind(U1, U2))
+
+  # Sample from the copula
+  copula_sample <- rCopula(length(Y1), fit@copula)
+
+  # Transform the samples back to original marginal distributions
+  Z1 <- as.numeric(quantile(Y1, copula_sample[,1]))
+  Z2 <- as.numeric(quantile(Y2, copula_sample[,2]))
+
+  # Construct the list of samples
+  zs <- lapply(1:length(Z1), function(i) c(site1 = Z1[i], site2 = Z2[i]))
+  return(zs)
+}
+
+ys$ys <- correlate_samples(ys$ys, rho = .6)
+
 ytot = map_vec(ys$ys, sum)
 p_y_tot <- data.frame(x = ytot) %>% ggplot(aes(x=x)) +
   geom_histogram() + labs(x = "ytot")
@@ -94,13 +126,13 @@ plot_dat <- allos %>%
 
 #Ks_to_show <- as.character(seq(150, 130, by = 20))
 Ks_to_show <- as.character(Ks)
-(p <- plot_dat %>% filter(K %in% c("mean CRPS", Ks_to_show), model != "dgp") %>%
-    ggplot() +
-    geom_density(aes(x = score, fill = model), color = NA, alpha = .5) +
-    facet_wrap(vars(K)) + theme_bw() + ylim(c(0,.15)))
-
-p + geom_density(data = plot_dat %>% filter(K %in% c("mean CRPS", Ks_to_show), model == "dgp"),
-                 aes(x = score), fill = NA)
+# (p <- plot_dat %>% filter(K %in% c("mean CRPS", Ks_to_show), model != "dgp") %>%
+#     ggplot() +
+#     geom_density(aes(x = score, fill = model), color = NA, alpha = .5) +
+#     facet_wrap(vars(K)) + theme_bw() + ylim(c(0,.15)))
+#
+# p + geom_density(data = plot_dat %>% filter(K %in% c("mean CRPS", Ks_to_show), model == "dgp"),
+#                  aes(x = score), fill = NA)
 
 p_ridges <- plot_dat %>% filter(K %in% c("mean CRPS", Ks_to_show)) %>% mutate(K = fct_rev(K)) %>%
   ggplot(aes(x = score, y = model, fill = model)) + geom_density_ridges(color = NA) +
