612fbb987f2ea0b3b8a2dde1edb2e91c9250d2b0
Aaron Gerding <agerding@umass.edu>
2023-12-03 15:32:57 -0500

    basic idea of iteration step

--- talks/plots/code/setup.R
+++ talks/plots/code/setup.R
@@ -24,26 +24,26 @@ tar_load(truth_data)
 tar_load(forecast_data)
 tar_load(all_alloscore_data)
 
-slim_dfs <- filter(all_alloscore_data, reference_date == "2021-12-27")
+forecasts_hosp <- slim_dfs <- filter(all_alloscore_data, reference_date == "2021-12-27")
 
-forecasts_hosp <- forecast_data %>%
-  bind_rows(
-    load_forecasts(
-      dates = "2021-12-27",
-      date_window_size = 6,
-      locations = "US",
-      types = c("quantile"),
-      targets = paste(0:30, "day ahead inc hosp"),
-      source = "local_hub_repo",
-      hub_repo_path = hub_repo_path,
-      verbose = FALSE,
-      as_of = NULL,
-      hub = c("US")) |>
-      align_forecasts() |>
-      dplyr::filter(
-        relative_horizon == 14)
-  ) %>%
-  filter(reference_date == "2021-12-27")
+# forecasts_hosp <- forecast_data %>%
+#   bind_rows(
+#     load_forecasts(
+#       dates = "2021-12-27",
+#       date_window_size = 6,
+#       locations = "US",
+#       types = c("quantile"),
+#       targets = paste(0:30, "day ahead inc hosp"),
+#       source = "local_hub_repo",
+#       hub_repo_path = hub_repo_path,
+#       verbose = FALSE,
+#       as_of = NULL,
+#       hub = c("US")) |>
+#       align_forecasts() |>
+#       dplyr::filter(
+#         relative_horizon == 14)
+#   ) %>%
+#   filter(reference_date == "2021-12-27")
 truth <- truth_data
 
 mkeep <- c("BPagano-RtDriven",
