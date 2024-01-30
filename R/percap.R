97ee76936c00a625ab744fe87f810a746893c584
Aaron Gerding <aaronger@users.noreply.github.com>
2024-01-29 20:13:28 -0500

    incorporate percap into a few more plots

--- R/percap.R
+++ R/percap.R
@@ -10,10 +10,13 @@ score_per_capita_allocation <- function(dat, pops, Kmax = 60000) {
 	  slice(1, .by = c(reference_date, K ,abbreviation)) %>% 
 	  mutate(model = "percap") %>% 
 	  left_join(pops[c('POPESTIMATE2021', 'abbreviation')], by = "abbreviation") %>% 
-	  mutate(popprop = POPESTIMATE2021 / sum(POPESTIMATE2021), 
-	         .before = x, .by = c(K, reference_date))  %>% 
-	  mutate(x = K * popprop, components_raw = pmax(y-x,0), 
-	         components = components_raw - components_oracle) %>% 
+	  group_by(K, reference_date) %>% 
+	  mutate(popprop = POPESTIMATE2021 / sum(POPESTIMATE2021), .before = x)  %>% 
+	  mutate(x = K * popprop, components_raw = pmax(y-x,0),
+	  	oracle = y * K / sum(y),
+	  	components_oracle = pmax(y - oracle, 0),
+	  	components = components_raw - components_oracle) %>% 
+	  ungroup() %>% 
 	  select(-popprop, -POPESTIMATE2021)
 
 	return(percap)
